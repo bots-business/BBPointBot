a461a9efcb13244c9aa5efdebee32334af1d5f78
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-06-20 16:24:15 +0530

    Create _setMasterRemoveableLimit.js

---
+++ commands/Master/_setMasterRemoveableLimit.js
@@ -0,0 +1,31 @@
+/*CMD
+  command: /setMasterRemoveableLimit
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: Master
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+if(!isAdmin){return}
+if(message == "/cancel"){
+Bot.sendMessage("Operantion Cancelled")
+return
+}
+
+if(!isNumeric(message)){
+Bot.sendMessage("Send only the amount in Number")
+return
+}
+
+var masterId = options.masterId
+var masterRemoveableLimit = Libs.ResourcesLib.anotherUserRes("masterRemoveableLimit",masterId);
+masterRemoveableLimit.set(parseInt(message))
+
+Bot.sendMessage("*Please enter the amount for Master Removeable Points per hours*\nFor the user :- `" +  masterId  + "`\n\nUse /cancel to cancel the command")
+Bot.run({
+command : "/setMasterRemoveableGrowthAmount",
+options : {masterId: masterId}
+})
