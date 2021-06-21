dfdd2a1951f0bb74f561ef57c26c3d3402bac298
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-06-21 08:43:04 +0530

    Update _setMasterRemoveableGrowthAmount.js

--- commands/Master/_setMasterRemoveableGrowthAmount.js
+++ commands/Master/_setMasterRemoveableGrowthAmount.js
@@ -9,20 +9,33 @@
   aliases: 
 CMD*/
 
-if(!isAdmin){return}
-if(message == "/cancel"){
-Bot.sendMessage("Operantion Cancelled")
-return
+if (!isAdmin) {
+  return
+}
+if (message == "/cancel") {
+  Bot.sendMessage("Operantion Cancelled")
+  return
 }
 
-if(!isNumeric(message)){
-Bot.sendMessage("Send only the amount in Number")
-return
+if (!isNumeric(message)) {
+  Bot.sendMessage("Send only the amount in Number")
+  return
 }
 
 var masterId = options.masterId
-var sec_in_hr = 1*60*60
-var masterRemoveablePoint = Libs.ResourcesLib.anotherUserRes("masterRemoveablePoint",masterId);
-masterRemoveablePoint.growth.add({value: parseInt(message) , interval: sec_in_hr});
+var sec_in_hr = 1 * 60 * 60
+var masterRemoveablePoint = Libs.ResourcesLib.anotherUserRes(
+  "masterRemoveablePoint",
+  masterId
+)
+masterRemoveablePoint.growth.add({
+  value: parseInt(message),
+  interval: sec_in_hr
+})
 
-Bot.sendMessage("*Master Removeale Growth Successfully Set for the User*` " + masterId + "`")
+Bot.sendMessage(
+  "Master Removeale Growth Successfully Set for the User " +
+    masterId +
+    "\n\n" +
+    masterRemoveablePoint.growth.title()
+)
