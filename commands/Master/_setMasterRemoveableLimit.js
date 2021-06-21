45f1bcba05cde960049e949b9d43de42b74b7590
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-06-21 08:43:58 +0530

    Update _setMasterRemoveableLimit.js

--- commands/Master/_setMasterRemoveableLimit.js
+++ commands/Master/_setMasterRemoveableLimit.js
@@ -9,23 +9,33 @@
   aliases: 
 CMD*/
 
-if(!isAdmin){return}
-if(message == "/cancel"){
-Bot.sendMessage("Operantion Cancelled")
-return
+if (!isAdmin) {
+  return
+}
+if (message == "/cancel") {
+  Bot.sendMessage("Operantion Cancelled")
+  return
 }
 
-if(!isNumeric(message)){
-Bot.sendMessage("Send only the amount in Number")
-return
+if (!isNumeric(message)) {
+  Bot.sendMessage("Send only the amount in Number")
+  return
 }
 
 var masterId = options.masterId
-var masterRemoveableLimit = Libs.ResourcesLib.anotherUserRes("masterRemoveableLimit",masterId);
+var masterRemoveableLimit = Libs.ResourcesLib.anotherUserRes(
+  "masterRemoveableLimit",
+  masterId
+)
 masterRemoveableLimit.set(parseInt(message))
 
-Bot.sendMessage("*Please enter the amount for Master Removeable Points per hours*\nFor the user :- `" +  masterId  + "`\n\nUse /cancel to cancel the command")
+Bot.sendMessage(
+  "*Please enter the amount for Master Removeavle Points per hours*\nFor the user :- `" +
+    masterId +
+    "`\n\nUse /cancel to cancel the command"
+)
 Bot.run({
-command : "/setMasterRemoveableGrowthAmount",
-options : {masterId: masterId}
+  command: "/setMasterRemoveableGrowthAmount",
+  options: { masterId: masterId }
 })
+
