948940b019ed8367d247cc171db89224e846a7b9
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-07-10 19:15:11 +0530

    Create _setAngryPoints.js

---
+++ commands/_setAngryPoints.js
@@ -0,0 +1,43 @@
+/*CMD
+  command: /setAngryPoints
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var AngryPointsdata = AdminPanel.getPanel("AngryPoints")
+
+var masterId = AngryPointsdata.fields[0].value
+var limit = AngryPointsdata.fields[1].value
+var growth = AngryPointsdata.fields[2].value
+var maxLimit = AngryPointsdata.fields[3].value
+
+var angryPointsLimit = Libs.ResourcesLib.anotherUserRes(
+  "angryPointsLimit",
+  masterId
+)
+AngryPointsLimit.set(parseInt(limit))
+
+var sec_in_hr = 1 * 60 * 60
+var angryPoints = Libs.ResourcesLib.anotherUserRes("angryPoints", masterId)
+angryPoints.growth.add({
+  value: parseInt(growth),
+  interval: sec_in_hr,
+  max: parseInt(maxLimit)
+})
+
+//making the admin panel empty so that bot admin can add more masters
+
+AngryPointsdata.fields[0].value = ""
+AngryPointsdata.fields[1].value = ""
+AngryPointsdata.fields[2].value = ""
+AngryPointsdata.fields[3].value = ""
+
+AdminPanel.setPanel({
+  panel_name: "AngryPoints",
+  data: AngryPointsdata
+})
