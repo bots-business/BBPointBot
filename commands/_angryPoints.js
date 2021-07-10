e3dd67adf2e01d61a6b3b3e4031ede8b2790f6eb
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-07-10 19:10:34 +0530

    Create _angryPoints.js

---
+++ commands/_angryPoints.js
@@ -0,0 +1,14 @@
+
+/*CMD
+  command: /angryPoints
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var angryPoints = Libs.ResourcesLib.userRes("AngryPoints")
+Bot.sendMessage("This Point can be used to remove the BB Points from the User\n\nYour Angry  Points :- " + angryPoints.value())
