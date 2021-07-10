78ddb27972f622ebc425bda3cb07a242e90be594
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-07-10 19:11:15 +0530

    Update _angryPoints.js

--- commands/_angryPoints.js
+++ commands/_angryPoints.js
@@ -10,5 +10,5 @@
   aliases: 
 CMD*/
 
-var angryPoints = Libs.ResourcesLib.userRes("AngryPoints")
+var angryPoints = Libs.ResourcesLib.userRes("angryPoints")
 Bot.sendMessage("This Point can be used to remove the BB Points from the User\n\nYour Angry  Points :- " + angryPoints.value())
