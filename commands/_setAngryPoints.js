002fbc1903b0f81233a08c840f4f2592f6b1c5a3
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-07-13 15:10:30 +0530

    Update _setAngryPoints.js

--- commands/_setAngryPoints.js
+++ commands/_setAngryPoints.js
@@ -12,12 +12,12 @@ CMD*/
 if(!user){return}
 //allowed to run through admin panel only
 
-var AngryPointsdata = AdminPanel.getPanel("AngryPoints")
+var angryPointsPanel = AdminPanel.getPanel("AngryPoints")
 
-var masterId = AngryPointsdata.fields[0].value
-var limit = AngryPointsdata.fields[1].value
-var growth = AngryPointsdata.fields[2].value
-var maxLimit = AngryPointsdata.fields[3].value
+var masterId = angryPointsPanel.fields[0].value
+var limit = angryPointsPanel.fields[1].value
+var hourlyGrowth = angryPointsPanel.fields[2].value
+var maxGrowthLimit = angryPointsPanel.fields[3].value
 
 var angryPointsLimit = Libs.ResourcesLib.anotherUserRes(
   "angryPointsLimit",
@@ -28,19 +28,19 @@ angryPointsLimit.set(parseInt(limit))
 var sec_in_hr = 1 * 60 * 60
 var angryPoints = Libs.ResourcesLib.anotherUserRes("angryPoints", masterId)
 angryPoints.growth.add({
-  value: parseInt(growth),
+  value: parseInt(hourlyGrowth),
   interval: sec_in_hr,
-  max: parseInt(maxLimit)
+  max: parseInt(maxGrowthLimit)
 })
 
 //making the admin panel empty so that bot admin can add more masters
 
-AngryPointsdata.fields[0].value = ""
-AngryPointsdata.fields[1].value = ""
-AngryPointsdata.fields[2].value = ""
-AngryPointsdata.fields[3].value = ""
+angryPointsPanel.fields[0].value = ""
+angryPointsPanel.fields[1].value = ""
+angryPointsPanel.fields[2].value = ""
+angryPointsPanel.fields[3].value = ""
 
 AdminPanel.setPanel({
   panel_name: "AngryPoints",
-  data: AngryPointsdata
+  data: angryPointsPanel
 })
