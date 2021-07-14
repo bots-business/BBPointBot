18ce60f40e0633592e0342edc1e9c8b59eed96b1
Soham Deogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-07-14 13:59:41 +0530

    Update _setAngryPoints.js

--- commands/_setAngryPoints.js
+++ commands/_setAngryPoints.js
@@ -12,23 +12,18 @@ CMD*/
 if(!user){return}
 //allowed to run through admin panel only
 
-var angryPointsPanel = AdminPanel.getPanel("AngryPoints")
-
-var masterId = angryPointsPanel.fields[0].value
-var maxLimit = angryPointsPanel.fields[1].value
-var hourlyGrowth = angryPointsPanel.fields[2].value
-var maxGrowthLimit = angryPointsPanel.fields[3].value
+var angryPointsPanel = AdminPanel.getPanelValues("AngryPoints")
 
 var angryPointsMaxLimit = Libs.ResourcesLib.anotherUserRes(
   "angryPointsMaxLimit",
-  masterId
+  angryPointsPanel.masterId
 )
-angryPointsMaxLimit.set(parseInt(maxLimit))
+angryPointsMaxLimit.set(parseInt(angryPointsPanel.maxLimit))
 
 var sec_in_hr = 1 * 60 * 60
-var availableAngryPoints = Libs.ResourcesLib.anotherUserRes("availableAngryPoints", masterId)
+var availableAngryPoints = Libs.ResourcesLib.anotherUserRes("availableAngryPoints", angryPointsPanel.masterId)
 availableAngryPoints.growth.add({
-  value: parseInt(hourlyGrowth),
+  value: parseInt(angryPointsPanel.hourlyGrowth),
   interval: sec_in_hr,
-  max: parseInt(maxGrowthLimit)
+  max: parseInt(angryPointsPanel.maxGrowthLimit)
 })
